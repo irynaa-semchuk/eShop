93eca909d66ce8a3cde9944b1b4381d1d5ed52c5
pKarpa <pavlo.karpa17@gmail.com>
2021-11-08 22:53:16 +0200

    Add models and create database.

---
+++ eShop/eShop/Models/Data/BlockedUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Models.Data
+{
+    public class BlockedUser
+    {
+        public int BlockedUserId { get; set; }
+        public string UserId { get; set; }
+    }
+}
