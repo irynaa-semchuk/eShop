b450146833ea2f8976e2334b6b62025a88d74d00
pKarpa <pavlo.karpa17@gmail.com>
2021-11-13 23:26:09 +0200

    Add interfaces BlockedUser and News

---
+++ eShop/eShop/Models/Interfaces/IBlockedUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using eShop.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Models.Interfaces
+{
+    interface IBlockedUserRepository
+    {
+        IQueryable<BlockedUser> BlockedUsers { get; }
+
+        void SaveBlockedUser(BlockedUser blockedUser);
+
+        BlockedUser DeleteBlockedUser(string blockedUserId);
+
+        BlockedUser GetBlockedUser(string blockedUserId);
+    }
+}
---
+++ eShop/eShop/Models/Interfaces/INewsRepository.cs
@@ -0,0 +1,21 @@
+﻿using eShop.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Models.Interfaces
+{
+    interface INewsRepository
+    {
+        IQueryable<News> NewsRepo { get; }
+
+        void SaveNews(News news);
+
+        News DeleteNews(int newsID);
+
+        News GetNews(int newsID);
+
+        IQueryable<News> LastNews();
+    }
+}
